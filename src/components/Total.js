df3f0a5d923f1cae4e3ec5ec28babb1d481d73f7
BuildTools <arthurpisakhov1@gmail.com>
2019-10-02 18:46:19 -0400

    price updates

--- src/components/Total.js
+++ src/components/Total.js
@@ -4,7 +4,10 @@ import { connect } from 'react-redux'
 const Total = props => {
   return (
     <div className="content">
-      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+      <h4>Total Amount: ${
+        props.car.price + props.additionalPrice
+        + props.car.features.reduce((acc, val) => acc + val.price, 0)
+      }</h4>
     </div>
   );
 };
